a5dd3cc2f6112842015a9c2ba3e9c944d958de1b
Jonas Myrlund <myrlund@gmail.com>
2013-10-24 13:54:43 +0200

    Added first iteration of python project.

---
+++ py-twitter/core.py
@@ -0,0 +1,105 @@
+# encoding: utf-8
+
+import sys
+import json
+import threading
+import operator
+import re
+
+from lib.datumbox import DatumBox
+from lib.twitter import Twitter, Tweet
+
+DEBUG = False
+
+CONFIG = json.loads(open('config.json', 'r').read())
+
+def get_tweets(query, result_type='popular'):
+    # Search the Twitter API for the given title
+    twitter_client = Twitter(CONFIG['twitter'])
+    tweet_data = twitter_client.search("\"%s\" -download -stream -#nw -#nowwatching -RT" % (query,), result_type=result_type, count=30)
+    
+    if DEBUG: print "Retrieved %d tweets." % len(tweet_data)
+    
+    # if DEBUG:
+    #     print json.dumps(tweet_data[0], sort_keys=True, indent=4, separators=(',', ': '))
+    
+    # Build simple Twitter objects
+    tweets = map(Tweet, tweet_data)
+    
+    # Add a filtered version of the tweet text
+    pattern = re.compile(query.replace(" ", " ?"), re.I)
+    for tweet in tweets:
+        tweet.filtered_text = pattern.sub('', tweet.text)
+    
+    return tweets
+
+def apply_sentiment(datumbox, tweet):
+    encoded_tweet = tweet.filtered_text.encode('utf8')
+    try:
+        tweet.sentiment = datumbox.twitter_sentiment_analysis(encoded_tweet)
+    except Exception as e:
+        print "This query spawned 500 error: %s" % encoded_tweet
+        print e
+
+def batches(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def get_sentiments(tweets):
+    # Sentiment analyze tweets
+    if DEBUG: print "Starting sentiment analysis."
+    datumbox = DatumBox(CONFIG['datumbox']['API_KEY'])
+    
+    for batch in batches(tweets, CONFIG['datumbox']['BATCH_SIZE']):
+        threads = [threading.Thread(target=apply_sentiment, args=(datumbox, tweet,)) for tweet in batch]
+        [thread.start() for thread in threads]
+        [thread.join() for thread in threads]
+        if DEBUG:
+            for tweet in batch:
+                print unicode(tweet)
+    
+    sentiments = [tweet.sentiment for tweet in tweets]
+    return sentiments
+
+def weigh_sentiments(sentiments):
+    # Filter out missing sentiment values
+    not_none = lambda sentiment: sentiment is not None
+    valid_sentiments = filter(not_none, sentiments)
+    
+    # Apply some weights to quantify result vector
+    if DEBUG: print "Applying weights."
+    
+    weights = {
+        'positive': 10,
+        'negative': -10,
+        'neutral': 2,
+    }
+    apply_weight = lambda sentiment: weights[sentiment]
+    weights = map(apply_weight, valid_sentiments)
+    
+    if DEBUG: print "Result vector: (%s)" % ", ".join(["%d" % weight for weight in weights])
+    
+    return tuple(weights)
+
+def analyze_title(title, source_type='popular'):
+    tweets = get_tweets(title, result_type=source_type)
+    sentiments = get_sentiments(tweets)
+    weights = weigh_sentiments(sentiments)
+    return weights
+
+def calculate_score(result):
+    return sum(result) / float(len(result))
+
+def average(s): return sum(s) * 1.0 / len(s)
+
+def title_score(title, heuristic='popular'):
+    result = analyze_title(title, source_type=heuristic)
+    score = calculate_score(result)
+    print "VARIANCE: %.2f" % average(map(lambda x: (x - score)**2, result))
+    return score
+
+if __name__ == '__main__':
+    titles = sys.argv[1:]
+    for title in titles:
+        print "Scoring %s." % title
+        print "SCORE: %.2f" % title_score(title, heuristic='mixed')
---
+++ py-twitter/lib/twitter.py
@@ -0,0 +1,96 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+import requests
+import json
+from datetime import date, timedelta
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
+
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+CONSUMER_KEY = "bERRpxqRNywXn2goGyDLA"
+CONSUMER_SECRET = "EesTZzoqKNXerlntfkmXNqnW5BKBvRjJIeoBtqOe2c"
+
+OAUTH_TOKEN = "14317755-wlQ7wAY2S5oGnHpVnpTuPEjhbZ73OBPUrDWCWyiC5"
+OAUTH_TOKEN_SECRET = "2mVNpK0PC45sKOK290oDBlYaDtzBMkeZR2qhnOGynQ"
+
+def setup_oauth(config):
+    """Authorize your app via identifier."""
+    # Request token
+    oauth = OAuth1(config['CONSUMER_KEY'], client_secret=config['CONSUMER_SECRET'])
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+
+    resource_owner_key = credentials.get('oauth_token')[0]
+    resource_owner_secret = credentials.get('oauth_token_secret')[0]
+
+    # Authorize
+    authorize_url = AUTHORIZE_URL + resource_owner_key
+    print 'Please go here and authorize: ' + authorize_url
+
+    verifier = raw_input('Please input the verifier: ')
+    oauth = OAuth1(config['CONSUMER_KEY'],
+                   client_secret=config['CONSUMER_SECRET'],
+                   resource_owner_key=resource_owner_key,
+                   resource_owner_secret=resource_owner_secret,
+                   verifier=verifier)
+
+    # Finally, Obtain the Access Token
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    token = credentials.get('oauth_token')[0]
+    secret = credentials.get('oauth_token_secret')[0]
+
+    return token, secret
+
+
+def get_oauth(config):
+    oauth = OAuth1(config['CONSUMER_KEY'],
+                client_secret=config['CONSUMER_SECRET'],
+                resource_owner_key=config['OAUTH_TOKEN'],
+                resource_owner_secret=config['OAUTH_TOKEN_SECRET'])
+    return oauth
+
+class Tweet:
+    FIELDS = ('id', 'text', 'lang')
+
+    def __init__(self, data):
+        for field in self.FIELDS:
+            setattr(self, field, data[field])
+        self.user = data['user']['screen_name']
+        self.data = data
+        self.sentiment = None
+        self.filtered_text = None
+
+    def __unicode__(self):
+        s = u""
+        if self.sentiment:
+            s = (u"<%s> " % self.sentiment).ljust(11)
+        return s + u"@%s: «%s»" % (self.user, self.text)
+
+class Twitter:
+    RESOURCE_URL_TEMPLATE = "https://api.twitter.com/1.1/%s.json"
+
+    def __init__(self, config):
+        self.oauth = get_oauth(config)
+    
+    def api_resource(self, resource):
+        return Twitter.RESOURCE_URL_TEMPLATE % resource
+
+    def api_request(self, resource, payload):
+        url = self.api_resource(resource)
+        r = requests.get(url=url, auth=self.oauth, params=payload)
+        return r.json()
+
+    def search(self, term, result_type='popular', count=10):
+        payload = {
+            'q': term,
+            'result_type': result_type,
+            'count': count,
+            'lang': 'en',
+        }
+        data = self.api_request("search/tweets", payload)
+        return data["statuses"]
